91e06ca03177899667be984e2e6062d98a09bbbd
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-04-25 12:21:03 -0400

    Make it so

---
+++ portfolio/portfolio_report.py
@@ -0,0 +1,25 @@
+"""
+Generates performance reports for your stock portfolio.
+"""
+
+
+def main():
+    """
+    Entrypoint into program.
+    """
+    pass
+
+
+def read_portfolio(filename):
+    """Returns data from a CSV file"""
+    # TODO: Read the CSV file with the filename above,
+    #       and return the data.
+
+
+def save_portfolio(data, filename):
+    """Saves data to a CSV file"""
+    # TODO: Save the provided data to the provided filename.
+
+
+if __name__ == '__main__':
+    main()
